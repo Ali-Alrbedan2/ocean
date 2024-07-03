f9d3b11834bea8771d69e7359c2055b015cbd5f9
Ali-Alrbedan2 <166887277+Ali-Alrbedan2@users.noreply.github.com>
2024-07-03 20:51:16 +0300

    Add files via upload

---
+++ Physic.js
@@ -0,0 +1,10 @@
+import "./style.css";
+// import * as dat from "dat.gui";
+import Vector3 from "./Library";
+import Diriction from "./Directions";
+import GMath from "./GMath";
+class Boat {
+  constructor() {
+  }
+}
+export default Boat;
